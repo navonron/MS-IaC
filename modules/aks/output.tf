9eadf62d6c4058f480caf3daeb7509e1faf5b660
ronnavon <nvonron@gmail.com>
2024-12-26 17:11:30 +0200

    Deploy Azure Resources

--- modules/aks/output.tf
+++ modules/aks/output.tf
@@ -6,10 +6,6 @@ output "aks_resources_rg" {
   value = azurerm_kubernetes_cluster.aks.node_resource_group
 }
 
-output "aks_nsg" {
-  value = azurerm_kubernetes_cluster.aks.node
-}
-
 output "host" {
   value = azurerm_kubernetes_cluster.aks.kube_config[0].host
 }
