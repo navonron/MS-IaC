07b29b507f477a60d966e385bc315913442da933
ronnavon <nvonron@gmail.com>
2024-12-26 13:30:44 +0200

    Deploy Azure Resources

--- env/dev/main.tf
+++ env/dev/main.tf
@@ -7,7 +7,7 @@ module "vnet" {
   source              = "../../modules/vnet"
   name                = "${var.env}-vnet"
   resource_group_name = module.resource_group.name
-  address_space       = [var.address_space]
+  address_space = [var.address_space]
 }
 
 data "azurerm_virtual_network" "mgm_vnet" {
@@ -21,7 +21,7 @@ module "vnet_peering_mgm_to_aks" {
   resource_group_name = data.azurerm_virtual_network.mgm_vnet.resource_group_name
   source_vnet_name    = data.azurerm_virtual_network.mgm_vnet.name
   remote_vnet_id      = module.vnet.id
-  depends_on          = [module.vnet]
+  depends_on = [module.vnet]
 }
 
 module "vnet_peering_aks_to_mgm" {
@@ -30,7 +30,7 @@ module "vnet_peering_aks_to_mgm" {
   resource_group_name = module.resource_group.name
   source_vnet_name    = module.vnet.name
   remote_vnet_id      = data.azurerm_virtual_network.mgm_vnet.id
-  depends_on          = [module.vnet]
+  depends_on = [module.vnet]
 }
 
 module "aks_subnet" {
@@ -38,21 +38,22 @@ module "aks_subnet" {
   name                 = "${var.env}-aks-snet"
   resource_group_name  = module.resource_group.name
   virtual_network_name = module.vnet.name
-  address_prefixes     = [cidrsubnet(var.address_space, 8, 0)]
-  depends_on           = [module.vnet]
+  address_prefixes = [cidrsubnet(var.address_space, 8, 0)]
+  depends_on = [module.vnet]
 }
 
 module "aks" {
   source              = "../../modules/aks"
   name                = "${var.env}-aks"
+  location            = "North Europe"
   resource_group_name = module.resource_group.name
   dns_prefix          = "${var.env}aks"
   default_node_pool = {
     name           = "np001"
     vnet_subnet_id = module.aks_subnet.id
   }
-  service_cidr       = cidrsubnet(var.address_space, 8, 1)
-  dns_service_ip     = cidrhost(cidrsubnet(var.address_space, 8, 1), 4)
+  service_cidr = cidrsubnet(var.address_space, 8, 1)
+  dns_service_ip = cidrhost(cidrsubnet(var.address_space, 8, 1), 4)
   nginx_ingress_name = "${var.env}-ingress"
 }
 
