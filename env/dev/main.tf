28899ff6d79190d5528abf654d3bb6d277248b4f
ronnavon <nvonron@gmail.com>
2024-12-28 12:57:06 +0200

    Deploy Azure Resources

--- env/dev/main.tf
+++ env/dev/main.tf
@@ -60,7 +60,7 @@ module "acr" {
   acr_token_name      = "${var.env}-github-runner-token"
   network_rule_set = {
     ip_rules = [{
-      ip_range = data.azurerm_virtual_network.mgm_vnet.address_space
+      ip_range = data.azurerm_virtual_network.mgm_vnet.address_space[0]
     }]
   }
 }
