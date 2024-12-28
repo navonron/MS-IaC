3d620db7c0c3a9c89ae75baf792ef2fd9597aa66
ronnavon <nvonron@gmail.com>
2024-12-28 12:53:13 +0200

    Deploy Azure Resources

--- modules/acr/main.tf
+++ modules/acr/main.tf
@@ -3,9 +3,16 @@ resource "azurerm_container_registry" "acr" {
   location            = var.location
   resource_group_name = var.resource_group_name
   sku                 = var.sku
-  network_rule_set = {
+
+  network_rule_set {
     default_action = var.network_rule_set.default_action
-    ip_rule        = var.network_rule_set.ip_rules
+
+    ip_rule = [
+      for rule in var.network_rule_set.ip_rules : {
+        action   = rule.action
+        ip_range = rule.ip_range
+      }
+    ]
   }
 }
 
